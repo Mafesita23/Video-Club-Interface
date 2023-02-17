e9ae9ecf89d864cce583a0cfe93a1820a2b1e435
DrJuanch <juanjo88023@gmail.com>
2023-02-17 15:51:55 -0500

    Doing the backend and frontend

--- Frontend/videoCLub/src/components/Forms.jsx/Movies.jsx
+++ Frontend/videoCLub/src/components/Forms.jsx/Movies.jsx
@@ -1,8 +1,34 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import swal from 'sweetalert'
 
-function Movies() {
+const movieList = ({item, infoMovie, setMovie, deleteMovies}) =>{
+
+  const fetchData = async () => {
+    const response = await fetch("",);
+    const data = await response.json();
+  }
+  fetchData();
+  setMovie(())
+
+
+  const handleDeleteMovie = () => {
+    swal({
+      title: "Estás seguro de borrar esta película? ",
+      buttons: true,
+      dangerMode: true, 
+    })
+    .then((deleteMovie) => {
+      if (deleteMovie){
+        swal("Película borrada exitosamente")
+        deleteMovies()
+      }else{
+        swal("Cancelaste la eliminación")
+      }
+    });
+  }
+
     const { id } = useParams();
     const [movie, setMovie] = useState([]);
 
@@ -16,14 +42,14 @@ console.log(movie);
 return (
   <>
     <div className="h-full w-full flex flex-col mt-32 justify-center items-center">
-      <Link
+      <Link>
         to={`/`}
         className="hover:bg-teal-600 bg-white hover:shadow-md  outline-none rounded-xl font-bold border mt-8 hover:text-teal-200 text-teal-600 border-zinc-400 py-4 px-4 pl-4"
-      >
         Back To Home
       </Link>
-      {movie && (
-        <div className="w-[700px] h-[200] px-6 py-4 flex shadow-xl rounded-xl justify-center items-center bg-teal-600 mt-16 border-teal-800 border-2">
+    </div>
+      {/* {movie && ( */}
+        {/* <div className="w-[700px] h-[200] px-6 py-4 flex shadow-xl rounded-xl justify-center items-center bg-teal-600 mt-16 border-teal-800 border-2">
           <div className="w-5/12 flex flex-col space-y-4">
             <h2 className="text-white font-bold text-3xl border-black border-b-2">
             Titulo
@@ -46,30 +72,51 @@ return (
           </div>
           <div className="w-7/12 flex flex-col space-y-4  ">
             <h2 className="text-teal-200 font-bold text-3xl border-black border-b-2">
-              {movie.title}
+              {item.title}
             </h2>
             <h2 className="text-teal-200 font-bold text-3xl border-black border-b-2">
-              {movie.year}
+              {item.year}
             </h2>
             <h2 className="text-teal-200 font-bold text-3xl border-black border-b-2">
-              {movie.time}
+              {item.time}
             </h2>
             <h2 className="text-teal-200 font-bold text-3xl border-black border-b-2">
-              {movie.language}
+              {item.language}
             </h2>
             <h2 className="text-teal-200 font-bold text-3xl border-black border-b-2">
-              {movie.date}
+              {item.date}
             </h2>
             <h2 className="text-teal-200 font-bold text-3xl border-black border-b-2">
-              {movie.country}
+              {item.country}
             </h2>
 
           </div>
-        </div>
-      )}
-    </div>
-  </>
-);
+        </div> */}
+        
+  <Table responsive="xl" bordered hover id="table">
+    <thead id="table-header">
+      <tr>
+        <th>Titulo pelicula</th>
+        <th>Año lanzamiento</th>
+        <th>Duracion en min</th>
+        <th>Lenguage</th>
+        <th>Fecha</th>
+        <th>País</th>
+        <th>Genero</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        infoMovie.map((item) =>{
+          return(
+            <MovieList item={item} key={item._id} setMovie={setMovie}/>
+          )
+        })
+      }
+    </tbody>
+  </Table>
+    </>
+  )
 }
 
 export default Movies;
